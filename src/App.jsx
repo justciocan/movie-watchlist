b09c8a6c7a91181b2b95b35e361090f059e69d49
Cosmin Ciocan <justciocan@gmail.com>
2026-01-27 17:06:30 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Movie Watchlist</h1>
+      <p>React + Firebase portfolio app.</p>
+    </div>
+  );
+}
+
+export default App;

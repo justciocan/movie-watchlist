3b4a4bed9cccd23758cbe121c5ed4af6dc759021
Cosmin Ciocan <justciocan@gmail.com>
2026-01-27 17:14:52 +0000

    clean up, folder structuring, home and base style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Movie Watchlist</h1>
-      <p>React + Firebase portfolio app.</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="app-container">
+      <h1>Movie Watchlist</h1>
+      <p>Search movies and manage your watchlist.</p>
+    </div>
+  );
+}
+
+export default Home;
